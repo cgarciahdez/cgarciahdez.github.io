98e89e2fe62d790bb675419b056ec280310b2293
cgarciahdez <c.garcia11@uniandes.edu.co>
2017-02-14 20:02:33 -0500

    about, art terminadas

--- js/freelancer.js
+++ js/freelancer.js
@@ -11,6 +11,14 @@
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
+    
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
